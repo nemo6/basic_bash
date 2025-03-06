c03d19a9fc27e8fef75e2d864409580ca2df2b1f
nemo6 <miguel.maudet.contact@gmail.com>
2025-03-06 16:31:33 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -5,7 +5,6 @@ alias du2='node "C:/Users/nemo6/Dropbox/E lab2/a.code/javascript/linux/du.js" "$
 alias du_hashx='node "C:/Users/nemo6/Dropbox/E lab2/a.code/javascript/linux/du_hashx.js" "$(pwd)"'
 alias nodejs_imagemagik_resize_50p='node "C:/Users/nemo6/Dropbox/E lab2/a.code/command/nodejs-command/nodejs-imagemagik-resize-50p/app.js" "$(pwd)"'
 alias compare_print='electron "C:/Users/nemo6/Dropbox/E lab2/a.code/command/nodejs-command/compare_print" "$(pwd)"'
-alias bash='/cygdrive/c/cygwin64/bin/bash'
 
 pwd () {
 	myVar="$PWD"
@@ -29,18 +28,18 @@ export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 
 HISTIGNORE='exit'
 
-num=$(cat C:/cygwin64/home/nemo6/file.txt | tr -cd '[:digit:].-')
-
-if [ $num -eq 0 ]; then
-	echo "first"
-	((num++))
-	echo $num > C:/cygwin64/home/nemo6/file.txt
-	exec bash --login
-else
-	echo "second"
-	echo "0" > C:/cygwin64/home/nemo6/file.txt
-fi
+# num=$(cat C:/cygwin64/home/Miguel/file.txt | tr -cd '[:digit:].-')
+# if [ $num -eq 0 ]; then
+	# echo "first"
+	# ((num++))
+	# echo $num > C:/cygwin64/home/Miguel/file.txt
+	# exec /cygdrive/c/cygwin64/bin/bash --login
+# else
+	# echo "second"
+	# echo "0" > C:/cygwin64/home/Miguel/file.txt
+# fi
 
 # exec bash --login
 # cd "/cygdrive/c/Users/nemo6/Desktop"
 # export HISTCONTROL=ignoreboth:erasedups
+# alias bash='/cygdrive/c/cygwin64/bin/bash'
